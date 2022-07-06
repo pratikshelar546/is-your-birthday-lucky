02e17a8f4d42c33544fed0174f2312f280c3f35e
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2022-07-06 18:18:32 +0530

    javascript done

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 const date = document.querySelector("#birthdate");
 const number = document.querySelector("#luckyno");
 const click = document.querySelector("#button");
+const msg = document.querySelector("#massage");
+
+
+
+function calculateSum(dob){
+    const birthdate = dob.replaceAll("-","");
+    console.log(birthdate);
+    let sum = 0;
+    for(let i=0; i<birthdate.length;i++){
+        sum = sum+ Number(birthdate.charAt(i));
+    }
+    checkLucky(sum);
+  
+}
+function checkLucky(sum){
+    const luckyNoIs = number.value;
+    console.log(sum,luckyNoIs);
+    const ans = sum % luckyNoIs;
+    console.log(ans);
+    if(ans === 0){
+        msg.innerText="yehh!!!Your birthdate is lucky 🎉";
+    }else{
+        msg.innerText="ummmm!!!Your birthdate is not lucky 😔";
+    }
+}
+
 
 click.addEventListener("click", function luckyOrNot(){
-//    const datee = date.value;
-    console.log(date.value);
-    console.log(number.value);
+const dob = date.value;
+  
+calculateSum(dob);
 
 })
