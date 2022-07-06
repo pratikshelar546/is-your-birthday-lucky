751123096824a146cebb82ec6a8d399a88816e77
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2022-07-06 14:39:55 +0530

    feat added html

---
+++ main.js
@@ -0,0 +1,10 @@
+const date = document.querySelector("#birthdate");
+const number = document.querySelector("#luckyno");
+const click = document.querySelector("#button");
+
+click.addEventListener("click", function luckyOrNot(){
+//    const datee = date.value;
+    console.log(date.value);
+    console.log(number.value);
+
+})
